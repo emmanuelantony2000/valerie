f052b007caa311cae9dd4a885c36e4f28f986c76
Alister Lee <alister@dev.shortepic.com>
2020-08-16 21:13:17 +1000

    More idiomatic types

--- examples/tictactoe/src/lib.rs
+++ examples/tictactoe/src/lib.rs
@@ -3,6 +3,52 @@ use valerie::prelude::*;
 use valerie::Channel;
 
 use futures_intrusive::channel::{shared::StateReceiver, StateId};
+use std::fmt::Display;
+use valerie::prelude::wasm_bindgen::__rt::core::fmt::Formatter;
+
+#[derive(Copy, Clone, PartialEq)]
+enum Square {
+    Empty,
+    X,
+    O,
+}
+
+impl Square {
+    pub fn rotate(self) -> Self {
+        match self {
+            Square::X => Square::O,
+            Square::O => Square::X,
+            _ => self
+        }
+    }
+}
+
+impl Display for Square {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        let s = match self {
+            Square::Empty => "",
+            Square::X => "X",
+            Square::O => "O",
+        };
+        write!(f, "{}", s)
+    }
+}
+
+#[derive(Copy, Clone, PartialEq)]
+enum Status {
+    Playing,
+    Won,
+}
+
+impl Display for Status {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        let s = match self {
+            Status::Playing => "Next player: ",
+            Status::Won => "Winner: ",
+        };
+        write!(f, "{}", s)
+    }
+}
 
 #[valerie(start)]
 pub fn run() {
@@ -22,21 +68,21 @@ fn game() -> Node {
 }
 
 fn board() -> Node {
-    let squares: [StateAtomic<&str>; 9] = [
+    let squares: [StateAtomic<Square>; 9] = [
         // Can't use array init shorthand because StateAtomic is not Copy
-        StateAtomic::new(""),
-        StateAtomic::new(""),
-        StateAtomic::new(""),
-        StateAtomic::new(""),
-        StateAtomic::new(""),
-        StateAtomic::new(""),
-        StateAtomic::new(""),
-        StateAtomic::new(""),
-        StateAtomic::new(""),
+        StateAtomic::new(Square::Empty),
+        StateAtomic::new(Square::Empty),
+        StateAtomic::new(Square::Empty),
+        StateAtomic::new(Square::Empty),
+        StateAtomic::new(Square::Empty),
+        StateAtomic::new(Square::Empty),
+        StateAtomic::new(Square::Empty),
+        StateAtomic::new(Square::Empty),
+        StateAtomic::new(Square::Empty),
     ];
 
-    let status = StateAtomic::new("Next player: ");
-    let next_player = StateAtomic::new("X");
+    let status = StateAtomic::new(Status::Playing);
+    let next_player = StateAtomic::new(Square::X);
 
     for square in squares.iter() {
         wasm_bindgen_futures::spawn_local(turn_checker(squares.to_vec(), square.rx(), next_player.clone(), status.clone()));
@@ -65,20 +111,20 @@ fn board() -> Node {
     ).into()
 }
 
-fn square(state: StateAtomic<&'static str>, status: StateAtomic<&'static str>, next_player: StateAtomic<&'static str>) -> Node {
+fn square(state: StateAtomic<Square>, status: StateAtomic<Status>, next_player: StateAtomic<Square>) -> Node {
     button!(state.clone())
         .class("square")
         .on_event("click", (), move |_, _| {
-            if status.value() == "Next player: " && state.value() == "" {
+            if status.value() == Status::Playing && state.value() == Square::Empty {
                 state.put(next_player.value());
             }
         })
         .into()
 }
 
-async fn turn_checker(squares: Vec<StateAtomic<&'static str>>, rx: StateReceiver<Channel>, next_player: StateAtomic<&'static str>, status: StateAtomic<&'static str>) {
+async fn turn_checker(squares: Vec<StateAtomic<Square>>, rx: StateReceiver<Channel>, next_player: StateAtomic<Square>, status: StateAtomic<Status>) {
 
-    fn calculate_winner(squares: &Vec<&str>) -> bool {
+    fn calculate_winner(squares: &Vec<Square>) -> bool {
         const LINES: [[usize; 3]; 8] = [
             [0, 1, 2],
             [3, 4, 5],
@@ -93,7 +139,7 @@ async fn turn_checker(squares: Vec<StateAtomic<&'static str>>, rx: StateReceiver
             let a = squares[*a];
             let b = squares[*b];
             let c = squares[*c];
-            if a != "" && a == b && a == c {
+            if a != Square::Empty && a == b && a == c {
                 return true;
             }
         }
@@ -102,15 +148,11 @@ async fn turn_checker(squares: Vec<StateAtomic<&'static str>>, rx: StateReceiver
 
     let mut old = StateId::new();
     while let Some((new, _)) = rx.receive(old).await {
-        let squares: Vec<&str> = squares.iter().map(|s| s.value()).collect();
+        let squares: Vec<Square> = squares.iter().map(|s| s.value()).collect();
         if calculate_winner(&squares) {
-            status.put("Winner: ");
+            status.put(Status::Won);
         } else {
-            next_player.put(match next_player.value() {
-                "X" => "O",
-                "O" => "X",
-                _ => panic!("Unexpected player"),
-            });
+            next_player.put(next_player.value().rotate());
         }
         old = new;
     }
